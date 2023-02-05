e9eb63af61c54d69b61e091b684e37d3c87a4d86
Jairop98 <jairop98@yahoo.com>
2023-02-04 19:01:01 -0800

    first commit

---
+++ src/main/java/com/jairop/javacafe/JavacafeApplication.java
@@ -0,0 +1,13 @@
+package com.jairop.javacafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavacafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jairop/javacafe/JavacafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jairop.javacafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavacafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
